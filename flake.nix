8271e95568caad329a0c192854b71cf5070c258b
Maya the bee <hello@unw.re>
2026-08-13 23:05:52 +0200

    flake: initialize a flake template with flake-parts

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "The various Nix crimes that make my Infrastructure work.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    disko.url = "github:nix-community/disko";
+  };
+
+  #> `flake-parts` is automagically included with magics of the flake-registry.
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    disko,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      flake = {
+        # In Greek mythology, Hypnos (/ˈhɪpnɒs/; Ancient Greek: Ὕπνος),
+        # also spelled Hypnus, is the personification of sleep.
+        nixosConfigurations.hypnos = nixpkgs.lib.nixosSystem {
+          modules = [];
+        };
+      };
+
+      systems = ["x86_64-linux"];
+      perSystem = {...}: {};
+    };
+}
